390253252b3ef151653f9e29c07c5cc7fa1183bf
dj940212 <2902273280@qq.com>
2017-08-02 15:40:47 +0800

    ..

--- webpack.config.js
+++ webpack.config.js
@@ -78,7 +78,7 @@ module.exports = {
                 loader: 'url-loader',
                 options: {
                     limit:10000,
-                    name:'../img/[name].[hash:7].[ext]'
+                    name:'img/[name].[hash:7].[ext]'
                 }
             }
 		]
